b7e1c8b872fd4a54a3001815e00395683b5bd6a2
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-11-01 14:31:14 +0200

    1-easy_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    let intervalId = null;
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
 
+    function changeBackgroundColor() {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }
+  
+    startButton.addEventListener('click', () => {
+      startButton.disabled = true;
+      stopButton.disabled = false;
+      intervalId = setInterval(changeBackgroundColor, 1000);
+    });
+  
+    stopButton.addEventListener('click', () => {
+      startButton.disabled = false;
+      stopButton.disabled = true;
+      clearInterval(intervalId);
+    });
+  });
+  
